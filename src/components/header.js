3debd86fd507cebddac4113cd78c6f8cf22d63f2
SorinTautu1991 <sorin.tautu@gmail.com>
2021-10-22 12:05:23 +0300

    Global Style *

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,7 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import styled from "styled-components"
+import { FaBars } from "react-icons/fa"
 
 const Header = () => {
   return (
@@ -30,4 +31,18 @@ const NavLink = styled(Link)`
   height: 100%;
   padding: 0 1rem;
   cursor: pointer;
+`
+
+const Bars = styled(FaBars)`
+  display: none;
+  color: #fff;
+  @media screen and (max-width: 768px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%, 75%);
+    font-size: 1.8rem;
+    cursor: pointer;
+  } ;
 `
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,11 @@
 import * as React from "react"
 import Header from "./Header"
+import { GlobalStyle } from "./styles/GlobalStyles"
 
 const Layout = ({ children }) => {
   return (
     <>
+      <GlobalStyle />
       <Header />
       <main>{children}</main>
     </>
