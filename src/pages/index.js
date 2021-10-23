37fbcd4aa10bb1037b3a958cc08e019234c99e15
SorinTautu1991 <sorin.tautu@gmail.com>
2021-10-23 19:53:12 +0300

    Added gatsby-transformer-json plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,13 @@ import * as React from "react";
 import Hero from "../components/Hero";
 import Layout from "../components/layout";
 import Seo from "../components/seo";
+import Trips from "../components/Trips";
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <Hero></Hero>
+    <Hero />
+    <Trips />
   </Layout>
 );
 
